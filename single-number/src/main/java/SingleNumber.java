453eb20ca620eb06fd5e3f84e469a34fdadf954a
ctam91 <ctam91@gmail.com>
2018-01-18 22:29:02 -0800

    improve solution

--- single-number/src/main/java/SingleNumber.java
+++ single-number/src/main/java/SingleNumber.java
@@ -1,8 +1,9 @@
+import java.util.Arrays;
 import java.util.HashMap;
 
 public class SingleNumber {
     public int findSingleNumber(int[] nums){
-        int result = 0;
+        /*int result = 0;
         if(nums.length==1){
             return nums[0];
         }
@@ -20,6 +21,16 @@ public class SingleNumber {
         for(int val : map.keySet()){
             if(map.get(val) == 1){
                 result = val;
+            }
+        }
+        return result;
+        */
+        int result = 0;
+
+        Arrays.sort(nums);
+        for(int i = 0; i < nums.length-1; i+=2){
+            if(nums[i] != nums[i+1]){
+                return nums[i];
             }
         }
         return result;
