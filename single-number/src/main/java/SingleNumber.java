94f3a4013c4bf504f50c9c00444cd5f78d6c5124
ctam91 <ctam91@gmail.com>
2018-01-18 22:35:25 -0800

    refactor code

--- single-number/src/main/java/SingleNumber.java
+++ single-number/src/main/java/SingleNumber.java
@@ -3,30 +3,7 @@ import java.util.HashMap;
 
 public class SingleNumber {
     public int findSingleNumber(int[] nums){
-        /*int result = 0;
-        if(nums.length==1){
-            return nums[0];
-        }
-
-        HashMap<Integer,Integer> map = new HashMap<Integer,Integer>();
-        for(int num : nums){
-            if(map.containsKey(num)){
-                map.put(num,2);
-            }
-            else{
-                map.put(num,1);
-            }
-        }
-
-        for(int val : map.keySet()){
-            if(map.get(val) == 1){
-                result = val;
-            }
-        }
-        return result;
-        */
         int result = 0;
-
         Arrays.sort(nums);
         for(int i = 0; i < nums.length-1; i+=2){
             if(nums[i] != nums[i+1]){
